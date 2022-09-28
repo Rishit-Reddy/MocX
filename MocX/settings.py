4083f3a0a7beb5b6f360f3f7fc9daf5ef0752aff
rishikiranap <86961915+rishikiranap@users.noreply.github.com>
2022-09-28 08:08:30 +0530

    commit

--- MocX/settings.py
+++ MocX/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k4+3t-5x@$l+!mw6^&to3lcacuv^$3op6*r@9&=21st)65tmly'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','mocx-app.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','nameless-ridge-99360.herokuapp.com']
 
 AUTH_USER_MODEL = "accounts.User"
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static'), ]
 
